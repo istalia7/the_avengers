445066d4948ddec5953a89038959ee283a542ec4
istalia7 <alexis.perrotin02@gmail.com>
2024-09-09 15:00:52 +0200

    header fix

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,6 @@
  * which should already be in your base.html.twig.
  */
 import "./styles/app.css";
-import "./styles/header.css";
 import "./styles/footer.css";
 
 console.log("This log comes from assets/app.js - welcome to AssetMapper! 🎉");
